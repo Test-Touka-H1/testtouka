07119376c18c0600b4653c5c9bd67cbbccd4f17f
Rovo Coding Agent <a393d78c-73d8-4f4d-84ea-500a2d3d8fc3@connect.atlassian.com>
2025-06-01 14:56:06 +0000

    SITCTMPL-1:  Remove unused matplotlib plotting functionality

--- run_plot.py
+++ run_plot.py
@@ -1,10 +1,2 @@
-import matplotlib.pyplot as plt
 import os
-
-def plot_data(data):
-    fig, ax = plt.subplots(figsize=(8,2))
-    ax.text(0, 1, data, fontsize=10, color='black', wrap=True, va='top', ha='left')
-    ax.axis('off')
-    plt.show()
-
 plot_data(os.uname())
