fd7eff4937303a88ecdbe8c47a3231d5ed379906
Rovo Coding Agent <a393d78c-73d8-4f4d-84ea-500a2d3d8fc3@connect.atlassian.com>
2025-06-01 15:17:51 +0000

    SITCTMPL-1:  refactor: improve username detection using environment variables

--- run_plot.py
+++ run_plot.py
@@ -1,5 +1,7 @@
 # importing os module
 import os
 
-# using getlogin() returning username
-os.getlogin()
+# using environ.get() method getting
+# current username
+print("USERNAME:", os.environ.get('USERNAME'))
+print("USER:", os.environ.get('USER'))
