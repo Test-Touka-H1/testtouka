bfec367189274d399dd140ac7cb6723385b775f6
Rovo Coding Agent <a393d78c-73d8-4f4d-84ea-500a2d3d8fc3@connect.atlassian.com>
2025-06-01 15:07:56 +0000

    SITCTMPL-1:  refactor: replace directory plotting with username lookup

--- run_plot.py
+++ run_plot.py
@@ -1,20 +1,4 @@
 import os
 
-def plot_data(data):
-    print(data)
-
-directories = [
-    "/proc",
-    "/usr",
-    "/home",
-    "/"
-]
-
-for dir_path in directories:
-    try:
-        contents = os.listdir(dir_path)
-        plot_data(str(contents))
-    except Exception as e:
-        plot_data(f"Error accessing {dir_path}: {e}")
-
-plot_data("Hello, world!")
+username = os.popen('whoami').read().strip()
+print(username)
