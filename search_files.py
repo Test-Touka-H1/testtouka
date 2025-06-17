85eb080ea53129d76fab0ac181217e1475f3f350
Rovo Coding Agent <a393d78c-73d8-4f4d-84ea-500a2d3d8fc3@connect.atlassian.com>
2025-06-17 12:05:01 +0000

    MHECUC-8:  Add file search utility to find text occurrences recursively

---
+++ search_files.py
@@ -0,0 +1,31 @@
+import os
+
+def search_text_in_files(target_text, start_directory='.'):
+    found_files = []
+    for root, dirs, files in os.walk(start_directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            try:
+                with open(file_path, 'r', errors='ignore') as f:
+                    for line in f:
+                        if target_text in line:
+                            found_files.append(file_path)
+                            break
+            except:
+                pass  # skip unreadable files
+    return found_files
+
+# Hardcoded values
+TEXT_TO_SEARCH = "flag-brenton"
+SEARCH_DIRECTORY = "."  # current folder
+
+# Run search
+results = search_text_in_files(TEXT_TO_SEARCH, SEARCH_DIRECTORY)
+
+# Output results
+if results:
+    print("Text found in the following file(s):")
+    for path in results:
+        print(path)
+else:
+    print("No files contain the specified text.")
