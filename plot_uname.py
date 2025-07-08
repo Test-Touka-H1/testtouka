614b7d8b6a147850b3b2a26fa11c5e4bcd85cad8
Rovo Coding Agent <a393d78c-73d8-4f4d-84ea-500a2d3d8fc3@connect.atlassian.com>
2025-07-08 11:42:41 +0000

    MHECUC-8:  Replace os.uname() with getpass.getuser() for username display
    - Switch from displaying full system info to just current username
    - Use getpass module for safer username retrieval
    - Fix code formatting (spacing in figsize tuple)
    - Update comments to reflect functionality change

--- plot_uname.py
+++ plot_uname.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
-import os
+import getpass  # safer for getting the current username
 
 def plot_data(data):
-    fig, ax = plt.subplots(figsize=(8,2))
+    fig, ax = plt.subplots(figsize=(8, 2))
     ax.text(0, 1, data, fontsize=10, color='black', wrap=True, va='top', ha='left')
     ax.axis('off')
     plt.show()
 
-plot_data(os.uname())
+# Replaces `os.uname()` with the current username
+plot_data(getpass.getuser())
